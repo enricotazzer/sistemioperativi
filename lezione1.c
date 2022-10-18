bb372ee088ff581a0e0b31870c9714261e9ae23d
enricotazzer <enrico.tappo00@gmail.com>
2022-10-18 11:33:18 +0200

    conversione fahrenheit-celsius

--- lezione1.c
+++ lezione1.c
@@ -1,7 +1,13 @@
 #inlude <stdio.h>
 
-int main(){
-    printf("hello, world! \n");
+#define LOWER 0
+#define UPPER 300
+#define STEP 20 
 
+int main(){
+    //printf("hello, world! \n");
+    for(float fahr = LOWER; fahr <= UPPER; fahr += STEP){
+        printf("%3.0f °F -> %6.1f °C\n", fahr, (5.0/9.0) * (fahr - 32));
+    }
     return 0;
 }
